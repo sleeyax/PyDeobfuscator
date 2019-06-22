2eaedc033c185cfe50b3c8fc46e0d35580328e39
Sleeyax <yourd3veloper@gmail.com>
2019-06-22 23:45:08 +0200

    Create output dirs if they don't exist

--- deobfuscator.py
+++ deobfuscator.py
@@ -3,14 +3,22 @@ import sys
 import glob
 from loader import args
 from modules import deobfuscators
-from logger import error, info
+from logger import error, info, debug
 
-print(args)
+debug(args)
 
+# exit if input file or directory doesn't exist
 if not os.path.exists(args.input):
     error('directory \'{0}\' does not exist!'.format(args.input))
     sys.exit(0)
 
+# recursively create output directories
+output_dir = os.path.dirname(args.output)
+if not os.path.exists(output_dir):
+    info('creating output directories...')
+    os.makedirs(output_dir)
+
+# recursively read all files in the input dir to an array
 input_files = [f for f in glob.glob('{0}/**/*.py'.format(args.input.rstrip('/')), recursive=True)] \
     if os.path.isdir(args.input) \
     else [args.input]
