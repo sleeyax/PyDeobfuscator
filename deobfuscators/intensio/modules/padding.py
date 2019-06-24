97f2effce11aba786ebeecb366441abda55c0830
Sleeyax <yourd3veloper@gmail.com>
2019-06-24 23:38:39 +0200

    Implemented process method

--- deobfuscators/intensio/modules/padding.py
+++ deobfuscators/intensio/modules/padding.py
@@ -7,13 +7,27 @@ class Padding(BaseModule):
     def __init__(self):
         self.length_levels = [32, 64, 128]
         self.pattern = self.build_pattern(self.length_levels)
+        self.trash = []
 
     def build_pattern(self, length_levels):
         parts = ['[a-zA-Z]{' + str(l) + '}' for l in length_levels]
         level_pattern = '|'.join(parts)
-        # ^((?:[a-zA-Z]{32}|[a-zA-Z]{64}|[a-zA-Z]{128})) = (?:'|\")(?:[a-zA-Z]{32}|[a-zA-Z]{64}|[a-zA-Z]{128})(?:'|\")$
-        return r"^((?:" + level_pattern + ")) = (?:'|\")(?:" + level_pattern + ")(?:'|\")$"
+        # ((?:[a-zA-Z]{32}|[a-zA-Z]{64}|[a-zA-Z]{128})) = (?:'|\")(?:[a-zA-Z]{32}|[a-zA-Z]{64}|[a-zA-Z]{128})(?:'|\")
+        return r"((?:" + level_pattern + ")) = (?:'|\")(?:" + level_pattern + ")(?:'|\")"
 
-    def process(self, line):
-        # TODO: implement
-        pass
+    def process(self, line: str):
+        # remove the line from output when it matches a fake variable
+        matches = [t for t in self.trash if t in line]
+        if len(matches) > 0:
+            return None
+
+        # if this line declares a fake variable, add it to the list & remove it from output
+        matches = re.search(self.pattern, line)
+        if matches:
+            trash = matches.group(1)
+            if trash not in self.trash:
+                self.trash.append(trash)
+            return None
+
+        # return line if it does not contain any signs of padding
+        return line
