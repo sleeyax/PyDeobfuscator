2d288e867c4dc6e38241b045b139398aaebc3731
Sleeyax <yourd3veloper@gmail.com>
2019-06-26 21:11:18 +0200

    Deobfuscate classes, methods, variables, for loop variables

--- deobfuscators/intensio/modules/declarations.py
+++ deobfuscators/intensio/modules/declarations.py
@@ -1,37 +1,46 @@
 import re
+
+from ..helpers import build_length_levels_pattern
 from .basemodule import BaseModule
 
 
 class Declarations(BaseModule):
     def __init__(self):
-        self.length_levels = [32, 64, 128]
-        self.length_levels_pattern = self.build_length_levels_pattern(self.length_levels)
-        self.variables_count = 0
+        self.characters_to_match = build_length_levels_pattern([32, 64, 128])
         self.variables = {}
         self.classes = {}
         self.methods = {}
+        self.for_loops = {}
 
-    def build_length_levels_pattern(self, length_levels):
-        parts = ['[a-zA-Z]{' + str(l) + '}' for l in length_levels]
-        return '|'.join(parts)
-
-    def build_variables_pattern(self, lengths_pattern):
-        # ^((?:[a-zA-Z]{32}|[a-zA-Z]{64}|[a-zA-Z]{128})+)\s*=
-        return r'^((?:' + lengths_pattern + ')+)\\s*='
+    def detect_declaration(self, line: str, pattern: str, collection: dict, keyword: str, auto_format: bool = True):
+        if auto_format:
+            pattern = pattern.format(self.characters_to_match)
+        match = re.match(pattern, line)
+        if match and match.group(1) not in collection.keys():
+            count = len(collection.items()) + 1
+            collection[match.group(1)] = keyword + str(count)
 
     def detect_variable_declaration(self, line: str):
-        pattern = self.build_variables_pattern(self.length_levels_pattern)
-        match = re.match(pattern, line)
-        if match and match.group(1) not in self.variables.keys():
-            self.variables_count += 1
-            self.variables[match.group(1)] = 'var' + str(self.variables_count)
+        self.detect_declaration(line, '^({0})\\s*=', self.variables, 'var')
+
+    def detect_class_declaration(self, line: str):
+        self.detect_declaration(line, 'class\\s+({0}?)(?:\\(|:)', self.classes, 'Class')
+
+    def detect_method_declaration(self, line: str):
+        self.detect_declaration(line, 'def\\s+({0})', self.methods, 'method')
+
+    def detect_for_loop_declaration(self, line: str):
+        self.detect_declaration(line, 'for\\s+({0})\\s+in', self.for_loops, 'i')
 
     def process(self, line: str):
         self.detect_variable_declaration(line)
+        self.detect_class_declaration(line)
+        self.detect_method_declaration(line)
+        self.detect_for_loop_declaration(line)
 
-        # TODO: detect class, method, loop, exception declaration
-
-        for key, value in self.variables.items():
-            line = line.replace(key, value)
+        collections = [self.variables, self.classes, self.methods, self.for_loops]
+        for collection in collections:
+            for key, value in collection.items():
+                line = line.replace(key, value)
 
         return line
