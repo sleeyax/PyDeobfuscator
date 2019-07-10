3a338d7ccda7d7a943ff04f5041eaf72c2f40522
Sleeyax <yourd3veloper@gmail.com>
2019-07-10 18:03:22 +0200

    Document method

--- modules/declarations/basedeclaration.py
+++ modules/declarations/basedeclaration.py
@@ -1,21 +1,29 @@
 import re
 from ..basemodule import BaseModule
-from deobfuscators.intensio.helpers import build_length_levels_pattern
 from abc import ABC, abstractmethod
 
 
 class BaseDeclaration(BaseModule, ABC):
-    def __init__(self):
-        self.characters_to_match = build_length_levels_pattern([32, 64, 128])
+    def detect_declaration(self, line: str, pattern: str, collection: dict, keyword: str, ai_keyword: bool = True, strip: bool = True):
+        """
+        Detects a declaration (i.e var, class, def, ...) in the specified line of text
 
-    def detect_declaration(self, line: str, pattern: str, collection: dict, keyword: str, auto_format: bool = True):
-        line = line.strip()
-        if auto_format:
-            pattern = pattern.format(self.characters_to_match)
+        :param line: line of text to search through
+        :param pattern: regex pattern to apply
+        :param collection: dictionary where a key : value pair of pattern match.group(1) : keyword will be saved
+        :param keyword: replacement value
+        :param ai_keyword: whether or not an auto incrementing integer should be appended to the keyword
+        :param strip: whether or not the line should be stripped of whitespaces first
+        :return:
+        """
+        line = line.strip() if strip else line
         match = re.match(pattern, line)
         if match and match.group(1) not in collection.keys():
-            count = len(collection.items()) + 1
-            collection[match.group(1)] = keyword + str(count)
+            if ai_keyword:
+                count = len(collection.items()) + 1
+                collection[match.group(1)] = keyword + str(count)
+            else:
+                collection[match.group(1)] = keyword
 
     @abstractmethod
     def process(self, line):
